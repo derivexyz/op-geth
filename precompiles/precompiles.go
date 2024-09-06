f751504c6ac9b799906c4670e3ce8f6fc8a725ec
jmoreira-ls <joao.moreira@lightshift.xyz>
2024-09-06 13:54:47 +0100

    Add support for different selectors

--- precompiles/precompiles.go
+++ precompiles/precompiles.go
@@ -7,11 +7,12 @@ package precompiles
 #include <stdint.h>
 
 uint64_t __precompile_black76_gas(const void* data_ptr, const uint32_t data_len);
-uint8_t __precompile_black76(const void* data_ptr, const uint32_t data_len, void* ret_val);
+uint8_t __precompile_black76(const void* data_ptr, const uint32_t data_len, void* ret_val, void* out_len);
 
 */
 import "C"
 import (
+	"fmt"
 	"unsafe"
 )
 
@@ -27,15 +28,25 @@ func (a *Black76) RequiredGas(input []byte) uint64 {
 }
 
 func (a *Black76) Run(input []byte) ([]byte, error) {
-	output := make([]byte, 48)
+	output := make([]byte, 1024)
 	cout := unsafe.Pointer(&output[0])
 
 	cstr := unsafe.Pointer(&input[0])
 	len := C.uint(len(input))
+	out_len := C.uint(0)
+	out_len_ptr := unsafe.Pointer(&out_len)
 
-	res := C.__precompile_black76(cstr, len, cout)
+	res := C.__precompile_black76(cstr, len, cout, out_len_ptr)
 
-	output[47] = byte(res)
+	output[1023] = byte(res)
 
-	return output, nil
+	output2 := make([]byte, out_len)
+	copy(output2, output[:out_len])
+
+	var err error = nil
+	if res != 0 {
+		err = fmt.Errorf("error: %d", res)
+	}
+
+	return output2[:], err
 }
--- precompiles/src/black76.rs
+++ precompiles/src/black76.rs
@@ -20,116 +20,21 @@ pub extern "C" fn __precompile_black76(
     data_ptr: *const u8,
     data_len: usize,
     ret_val: *mut u8,
+    ret_len: *mut usize,
 ) -> u8 {
     let data = unsafe { slice::from_raw_parts(data_ptr, data_len) };
-    let ret = unsafe { slice::from_raw_parts_mut(ret_val, 48) };
 
-    match compute(data, ret) {
-        Ok(()) => 0,
+    match compute(data) {
+        Ok(v) => {
+            let ret = unsafe { slice::from_raw_parts_mut(ret_val, v.len()) };
+            ret.copy_from_slice(&v);
+            unsafe {
+                *ret_len = v.len();
+            };
+            0
+        },
         Err(e) => e.code(),
     }
-}
-
-pub fn compute(data: &[u8], ret: &mut [u8]) -> Result<()> {
-    if data.len() < 4 {
-        return Err(Error::WrongSelectorLength);
-    }
-
-    let mut args = data;
-    if args.len() > 61 {
-        args = &args[4..]
-    }
-    if args.len() != 61 {
-        return Err(Error::WrongLengthOfArguments);
-    }
-
-    let (exponent, expiry_sec, discount, vol, fwd, strike) = extract_arguments(args);
-    // println!("EXPONENT {} | EXPIRY_SEC {} | DISCOUNT {} | VOL {} | FWD {} | STRIKE {}", exponent, expiry_sec, discount, vol, fwd, strike);
-
-    let fwd_discounted = fwd * discount;
-    if strike <= 0.0 {
-        write_result(
-            utils::f64_to_bytes(fwd_discounted, exponent),
-            utils::ZERO_BYTES,
-            utils::f64_to_bytes(discount, exponent),
-            ret,
-        );
-        return Ok(());
-    }
-
-    let strike_discounted = strike * discount;
-    if fwd <= 0.0 {
-        write_result(
-            utils::ZERO_BYTES,
-            utils::f64_to_bytes(strike_discounted, exponent),
-            utils::ZERO_BYTES,
-            ret,
-        );
-        return Ok(());
-    }
-
-    let mut black76 = Black76 {
-        strike,
-        expiry_sec,
-        is_call: true,
-    };
-
-    let call_price = black76.price(fwd, vol);
-    let call_delta = black76.delta(fwd, vol);
-    black76.is_call = false;
-    let put_price = black76.price(fwd, vol);
-
-    // let call_price = call_price * fwd_discounted;
-    // let put_price = put_price * fwd_discounted;
-    let call_delta = call_delta * discount;
-
-    let call_price = if call_price > fwd_discounted {
-        fwd_discounted
-    } else {
-        call_price
-    };
-    let put_price = if put_price > strike_discounted {
-        strike_discounted
-    } else {
-        put_price
-    };
-
-    write_result(
-        utils::f64_to_bytes(call_price, exponent),
-        utils::f64_to_bytes(put_price, exponent),
-        utils::f64_to_bytes(call_delta, exponent),
-        ret,
-    );
-    Ok(())
-}
-
-fn extract_arguments(args: &[u8]) -> (i8, f64, f64, f64, f64, f64) {
-    let exponent = i8::from_be_bytes(args[60..].try_into().unwrap());
-    let multiplier = BigFloat::from_u8(10).pow(&BigFloat::from_i8(-exponent));
-    let expiry_sec = u32::from_be_bytes(args[..4].try_into().unwrap());
-    let expiry_sec = BigFloat::from_u32(expiry_sec).to_f64();
-    let discount = u64::from_be_bytes(args[4..12].try_into().unwrap());
-    let discount = BigFloat::from_u64(discount).mul(&multiplier).to_f64();
-    let vol = u128::from_be_bytes(args[12..28].try_into().unwrap());
-    let vol = BigFloat::from_u128(vol).mul(&multiplier).to_f64();
-    let fwd = u128::from_be_bytes(args[28..44].try_into().unwrap());
-    let fwd = BigFloat::from_u128(fwd).mul(&multiplier).to_f64();
-    let strike = u128::from_be_bytes(args[44..60].try_into().unwrap());
-    let strike = BigFloat::from_u128(strike).mul(&multiplier).to_f64();
-    (exponent, expiry_sec, discount, vol, fwd, strike)
-}
-
-fn write_result(call_price: [u8; 16], put_price: [u8; 16], call_delta: [u8; 16], ret: &mut [u8]) {
-    // let cp = u128::from_be_bytes(call_price);
-    // let pp = u128::from_be_bytes(put_price);
-    // let cd = u128::from_be_bytes(call_delta);
-    // println!("CALL PRICE: {} | PUT PRICE: {} | CALL DELTA: {}", cp, pp, cd);
-
-    let mut res = [0; 48];
-    res[..16].copy_from_slice(&call_price);
-    res[16..32].copy_from_slice(&put_price);
-    res[32..].copy_from_slice(&call_delta);
-    ret.copy_from_slice(&res);
 }
 
 pub fn normcdf(x: f64) -> f64 {
@@ -191,5 +96,123 @@ impl Black76 {
         } else {
             normcdf(d1) - 1.0
         }
+    }
+}
+
+fn extract_arguments(args: &[u8]) -> (i8, f64, f64, f64, f64, f64) {
+    let exponent = i8::from_be_bytes(args[60..].try_into().unwrap());
+    let multiplier = BigFloat::from_u8(10).pow(&BigFloat::from_i8(-exponent));
+    let expiry_sec = u32::from_be_bytes(args[..4].try_into().unwrap());
+    let expiry_sec = BigFloat::from_u32(expiry_sec).to_f64();
+    let discount = u64::from_be_bytes(args[4..12].try_into().unwrap());
+    let discount = BigFloat::from_u64(discount).mul(&multiplier).to_f64();
+    let vol = u128::from_be_bytes(args[12..28].try_into().unwrap());
+    let vol = BigFloat::from_u128(vol).mul(&multiplier).to_f64();
+    let fwd = u128::from_be_bytes(args[28..44].try_into().unwrap());
+    let fwd = BigFloat::from_u128(fwd).mul(&multiplier).to_f64();
+    let strike = u128::from_be_bytes(args[44..60].try_into().unwrap());
+    let strike = BigFloat::from_u128(strike).mul(&multiplier).to_f64();
+    (exponent, expiry_sec, discount, vol, fwd, strike)
+}
+
+fn calculate_black76(args: &[u8]) -> Result<([u8; 16], [u8; 16], [u8; 16])> {
+    if args.len() != 61 {
+        return Err(Error::WrongLengthOfArguments);
+    }
+
+    let (exponent, expiry_sec, discount, vol, fwd, strike) = extract_arguments(args);
+    // println!("EXPONENT {} | EXPIRY_SEC {} | DISCOUNT {} | VOL {} | FWD {} | STRIKE {}", exponent, expiry_sec, discount, vol, fwd, strike);
+
+    let fwd_discounted = fwd * discount;
+    if strike <= 0.0 {
+        return Ok((
+            utils::f64_to_bytes(fwd_discounted, exponent),
+            utils::ZERO_BYTES,
+            utils::f64_to_bytes(discount, exponent),
+        ));
+    }
+
+    let strike_discounted = strike * discount;
+    if fwd <= 0.0 {
+        return Ok((
+            utils::ZERO_BYTES,
+            utils::f64_to_bytes(strike_discounted, exponent),
+            utils::ZERO_BYTES,
+        ));
+    }
+
+    let mut black76 = Black76 {
+        strike,
+        expiry_sec,
+        is_call: true,
+    };
+
+    let call_price = black76.price(fwd, vol);
+    let call_delta = black76.delta(fwd, vol);
+    black76.is_call = false;
+    let put_price = black76.price(fwd, vol);
+
+    // let call_price = call_price * fwd_discounted;
+    // let put_price = put_price * fwd_discounted;
+    let call_delta = call_delta * discount;
+
+    let call_price = if call_price > fwd_discounted {
+        fwd_discounted
+    } else {
+        call_price
+    };
+    let put_price = if put_price > strike_discounted {
+        strike_discounted
+    } else {
+        put_price
+    };
+
+    Ok((
+        utils::f64_to_bytes(call_price, exponent),
+        utils::f64_to_bytes(put_price, exponent),
+        utils::f64_to_bytes(call_delta, exponent),
+    ))
+}
+
+fn prices_delta(args: &[u8]) -> Result<Vec<u8>> {
+    let (call_price, put_price, call_delta) = calculate_black76(args)?;
+    let mut res = call_price.to_vec();
+    res.extend_from_slice(&put_price);
+    res.extend_from_slice(&call_delta);
+
+    Ok(res)
+}
+
+fn prices(args: &[u8]) -> Result<Vec<u8>> {
+    let (call_price, put_price, _) = calculate_black76(args)?;
+    let mut res = call_price.to_vec();
+    res.extend_from_slice(&put_price);
+
+    Ok(res)
+}
+
+fn delta(args: &[u8]) -> Result<Vec<u8>> {
+    let (_, _, call_delta) = calculate_black76(args)?;
+    let res = call_delta.to_vec();
+
+    Ok(res)
+}
+
+// prices_delta(uint32,uint64,uint128,uint128,uint128,int8)(uint128,uint128,uint128) 0x5f53183d
+pub const BLACK76_PRICES_DELTA_SELECTOR: [u8; 4] = [0x5f, 0x53, 0x18, 0x3d];
+// prices(uint32,uint64,uint128,uint128,uint128,int8)(uint128,uint128) 0x10251f08
+pub const BLACK76_PRICES_SELECTOR: [u8; 4] = [0x10, 0x25, 0x1f, 0x08];
+// delta(uint32,uint64,uint128,uint128,uint128,int8)(uint128) 0x129ab31e
+pub const BLACK76_DELTA_SELECTOR: [u8; 4] = [0x12, 0x9a, 0xb3, 0x1e];
+
+pub fn compute(data: &[u8]) -> Result<Vec<u8>> {
+    if data.len() < 4 {
+        return Err(Error::WrongSelectorLength);
+    }
+    match [data[0], data[1], data[2], data[3]] {
+        BLACK76_PRICES_DELTA_SELECTOR => prices_delta(&data[4..]),
+        BLACK76_PRICES_SELECTOR => prices(&data[4..]),
+        BLACK76_DELTA_SELECTOR => delta(&data[4..]),
+        _ => return Err(Error::UnknownSelector),
     }
 }
